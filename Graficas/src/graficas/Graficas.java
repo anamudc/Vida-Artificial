5871f0c4f731b752e055ca5b87ad746cec560521
Angie Amud <contaamud@icetex.gov.co>
2018-05-19 16:34:00 -0500

    commit inicial proyecto Graficas

---
+++ Graficas/src/graficas/Graficas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficas;
+
+/**
+ *
+ * @author angie
+ */
+public class Graficas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
